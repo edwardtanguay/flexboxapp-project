face277f08d26e536fe9b7a634d6db66dbfd38bc
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-02-04 11:04:31 +0100

    define state variables

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+	// define state variables
+	const [flexDirection, setFlexDirection] = useState('row'); // column, row (radio)
+	const [alignItems, setAlignItems] = useState('center'); // flex-start, flex-end, center, stretch, baseline
+	const [justifyContent, setJustifyContent] = useState('space-between'); // start, end, center, space-between, space-evenly, space-around
   return (
     <div className="App">
-	hello
+	hello2
     </div>
   );
 }
