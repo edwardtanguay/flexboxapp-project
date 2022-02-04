9b93d00846e4c9f67ac4b6675f04c282d4926a35
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-02-04 11:48:07 +0100

    flexDirection radio button works

--- src/App.js
+++ src/App.js
@@ -6,11 +6,18 @@ function App() {
 	const [flexDirection, setFlexDirection] = useState('row'); // column, row (radio)
 	const [alignItems, setAlignItems] = useState('center'); // flex-start, flex-end, center, stretch, baseline
 	const [justifyContent, setJustifyContent] = useState('space-between'); // start, end, center, space-between, space-evenly, space-around
-  return (
-    <div className="App">
-	hello2
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Flexbox Generator</h1>
+			<fieldset className="flexDirection">
+				<legend>Flex Direction</legend>
+				<input id="row" type="radio" name="flexDirection" checked={flexDirection === "row"} onChange={() => setFlexDirection('row')} />
+				<label htmlFor="row">row</label>
+				<input id="column" type="radio" name="flexDirection" checked={flexDirection === "column"} onChange={() => setFlexDirection('column')} />
+				<label htmlFor="column">column</label>
+			</fieldset>
+		</div>
+	);
 }
 
 export default App;
